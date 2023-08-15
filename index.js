e6edd2efc6bdd249f1801d4b22f8c07cb4545230
Harshit-Rajput20 <harshitrajput7782gmail.com>
2023-08-15 23:28:18 +0530

    fetched data using weather api and added to DOM

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const sunElement = document.getElementById("sun");
 const moonElement = document.getElementById("moon");
 const starsContainer = document.getElementById("stars"); // Stars container
-const timeElement = document.getElementById("time")
+const timeElement = document.getElementById("time");
 
 const centerXPercent = 50; // Center's x-coordinate percentage
 const centerYPercent = 90; // Center's y-coordinate percentage
@@ -23,6 +23,29 @@ function interpolateColor(color1, color2, percentage) {
   const b = Math.round(color1[2] + (color2[2] - color1[2]) * percentage);
   return `rgb(${r}, ${g}, ${b})`;
 }
+
+const changeTheme = () => {
+  const GStyles = document.getElementById("globalStyle");
+  if (GStyles.innerHTML.includes("color: #fff;")) {
+    GStyles.innerHTML = `* {
+      margin: 0;
+      color: #000;
+      padding: 0;
+      box-sizing: border-box;
+      /* color: #313030; */
+      overflow: hidden;
+    }`;
+  } else {
+    GStyles.innerHTML = `* {
+      margin: 0;
+      color: #fff;
+      padding: 0;
+      box-sizing: border-box;
+      /* color: #313030; */
+      overflow: hidden;
+    }`;
+  }
+};
 
 // Function to update the positions and background color based on the given percentage
 function updatePositionsAndBackgroundColor(percentage) {
@@ -53,7 +76,6 @@ function updatePositionsAndBackgroundColor(percentage) {
     bgColor = interpolateColor(color75, color100, (percentage - 75) / 25); // Interpolate between color75 and color100
   }
 
-
   const bodyElement = document.body;
 
   isSunUp = moonY > 100;
@@ -67,17 +89,15 @@ function updatePositionsAndBackgroundColor(percentage) {
     starsContainer.style.opacity = 1; // Show stars during the night
   }
 
-
   // Update the background color
   bodyElement.style.backgroundColor = bgColor;
 }
 
-
 function getCurrentTime() {
   const currentDate = new Date();
-  const hours = String(currentDate.getHours()).padStart(2, '0');
-  const minutes = String(currentDate.getMinutes()).padStart(2, '0');
-  const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+  const hours = String(currentDate.getHours()).padStart(2, "0");
+  const minutes = String(currentDate.getMinutes()).padStart(2, "0");
+  const seconds = String(currentDate.getSeconds()).padStart(2, "0");
   const currentTime = `${hours}:${minutes}:${seconds}`;
   return currentTime;
 }
@@ -125,7 +145,7 @@ function animateSunAndMoon(timestamp) {
   // Calculate the completion percentage
   completionPercentage = (elapsedTime / animationDuration) * 100;
   updatePositionsAndBackgroundColor(calculatePercentageSince6PM());
-  timeElement.innerHTML="<div  > Time "+getCurrentTime()+"</div>"
+  timeElement.innerHTML = "<div  > Time " + getCurrentTime() + "</div>";
   // Continue the animation until the duration is reached
   if (elapsedTime < animationDuration) {
     requestAnimationFrame(animateSunAndMoon);
@@ -141,3 +161,87 @@ function animateSunAndMoon(timestamp) {
 let startTime;
 const animationDuration = 15000; // 15 seconds
 requestAnimationFrame(animateSunAndMoon);
+
+//----------------------------------------------------------------------------------------------------------------------//
+
+//------------------------------------------------------------------------------------------------------------------------//
+
+const key = "d9e0fc277daee1e1a9dad1c05377a8ec";
+
+const searchBtn = document.getElementById("search-btn");
+const searchInp = document.getElementById("search");
+const country = document.getElementById("country");
+const temperature = document.getElementById("temperature");
+const tempImg = document.getElementById("temp-img");
+const realFeel = document.getElementById("info-2");
+const sunRise = document.getElementById("sun-rise");
+const sunSet = document.getElementById("sun-set");
+const latitude = document.getElementById("lat");
+const longitude = document.getElementById("long");
+const windSpeed = document.getElementById("wind-speed");
+const windDegree = document.getElementById("wind-degree");
+const description = document.getElementById("des-status");
+const minTemp = document.getElementById("min");
+const maxTemp = document.getElementById("max");
+const humidity = document.getElementById("humidity-info");
+const visibility = document.getElementById("visibility-info");
+const pressure = document.getElementById("pressure-info");
+const ground = document.getElementById("ground-info");
+const sea = document.getElementById("sea-info");
+const temperatureImg = document.getElementById("temperature");
+const mainTemp = document.getElementById("main-temp");
+
+const EpochConverrter = (epTime) => {
+  let d = new Date(0);
+  d.setUTCSeconds(epTime);
+  return d;
+};
+searchBtn.addEventListener("click", () => {
+  const cityValue = searchInp.value;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      country.innerText = data.name;
+      temperatureImg.innerHTML = `<img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png"> ${data.main.temp}`;
+      realFeel.innerText = `Real feel: ${data.main.feels_like}`;
+      sunRise.innerText = `Sunrise: ${
+        EpochConverrter(data.sys.sunrise)
+          .toTimeString()
+          .split(" ")[0]
+          .split(":")[0]
+      } : ${
+        EpochConverrter(data.sys.sunrise)
+          .toTimeString()
+          .split(" ")[0]
+          .split(":")[1]
+      }   AM`;
+      sunSet.innerText = `Sunset: ${
+        EpochConverrter(data.sys.sunset)
+          .toTimeString()
+          .split(" ")[0]
+          .split(":")[0]
+      } : ${
+        EpochConverrter(data.sys.sunset)
+          .toTimeString()
+          .split(" ")[0]
+          .split(":")[1]
+      }   PM`;
+      latitude.innerText = `Lattitude: ${data.coord.lat}`;
+      longitude.innerText = `Longitude: ${data.coord.lon}`;
+      windSpeed.innerText = `Wind Speed: ${data.wind.speed}`;
+      windDegree.innerText = `Wind Direction: ${data.wind.deg}`;
+      description.innerText = `${data.weather[0].description}`;
+      minTemp.innerText = `Min-temp: ${data.main.temp_min}`;
+      maxTemp.innerText = `Max-temp: ${data.main.temp_max}`;
+      humidity.innerText = `${data.main.humidity}`;
+      visibility.innerText = data.visibility;
+      pressure.innerText = data.main.pressure;
+      ground.innerText = data.main.grnd_level;
+      sea.innerText = data.main.sea_level;
+    })
+    .catch((error) => {
+      country.innerText = "Data not found";
+    });
+});
